2ffc197722d48869fa38e2d5defc4d0583ee2e13
Abdul Ajees S <108514085+Ajees008@users.noreply.github.com>
2024-08-03 18:00:16 +0530

    Add files via upload

---
+++ src/jokesData.js
@@ -0,0 +1,78 @@
+export default
+
+ [
+ 
+    {
+      "id": "01",
+      "setup": "ஒரு பையன் தலைக்கு அடியில் Dictionary வெச்சிட்டு தூக்குறானாம். ஏன்?",
+      "punchline": "ஏனா, அவனுக்கு அர்த்தமில்லாத கனவு வருதாம்.",
+      "rating": 4.5,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "02",
+      "setup": "கிணத்துல கல்லை போட்டால் ஏன் முழுகிறது?",
+      "punchline": "ஏன்னா, கல்லுக்கு நீச்சல் தெரியாதாம்.",
+      "rating": 3.8,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "03",
+      "setup": "ஒருத்தவங்க Sugar டப்பாவில், Saltனு எழுதி வெச்சாங்க ஏன்?",
+      "punchline": "எல்லா எருமையும் ஏமாத்த.",
+      "rating": 4.2,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "04",
+      "setup": "ஒரு Inspector தேங்காய் எடுத்துக்கிட்டு கைதியை பார்க்க போனாராம்... ஏன்?",
+      "punchline": "துருவி துருவி கேள்வி கேட்க தான்.",
+      "rating": 3.8,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "05",
+      "setup": "எந்த ஊருக்கு Award கொடுத்து இருக்காங்க?",
+      "punchline": "விருது(Award) நகர். ",
+      "rating": 4.1,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "06",
+      "setup": "ஒரு பறவை எழுதி கொண்டே இருக்கும் அது என்ன பறவை?",
+      "punchline": "பென்குயின். ஏன்னா அதுல பென் இருக்குல ",
+      "rating": 4.1,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "07",
+      "setup": "Weight இல்லாத House எது?",
+      "punchline": "Light House",
+      "rating": 4.1,
+      "attribution": "Madurai Muthu"
+    } ,
+    {
+      "id": "08",
+      "setup": "எல்லா Letter-ம் வர மாதிரி ஒரு Word சொல்லுங்க?",
+      "punchline": "Postbox",
+      "rating": 4.1,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "09",
+      "setup": "ஒரு பறவை எழுதி கொண்டே இருக்கும் அது என்ன பறவை??",
+      "punchline": "பென்குயின். ஏன்னா அதுல பென் இருக்குல.",
+      "rating": 4.1,
+      "attribution": "Madurai Muthu"
+    },
+    {
+      "id": "10",
+      "setup": "ஒரு Inspector தேங்காய் எடுத்துக்கிட்டு கைதியை பார்க்க போனாராம்... ஏன்??",
+      "punchline": "துருவி துருவி கேள்வி கேட்க தான். ",
+      "rating": 4.2,
+      "attribution": "Madurai Muthu"
+    }
+  
+  
+    
+  ]
